f4ac708160daa54d30185a5f346e493d13d2f12f
Vincent Tsang <vincent_tsang2017@outlook.com>
2022-01-16 18:05:17 +0800

    Initial Commit

---
+++ Jiwei/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jiwei
+//
+//  Created by Vincent Tsang on 16/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
